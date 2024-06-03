4893222eb2fbdb75542f2dc6a98dd6e6bf539f4d
Matthew Zenn <spyguy57@gmail.com>
2024-06-03 17:25:29 -0400

    Initialized site

---
+++ main.js
@@ -0,0 +1,24 @@
+const textarea = document.getElementById('editor');
+const numbers = document.getElementById("numbers");
+
+document.getElementById('copy').addEventListener('click', function() {
+  navigator.clipboard.writeText(textarea.value);
+});
+
+textarea.addEventListener("keyup", (e) => {
+  const num = e.target.value.split("\n").length;
+  numbers.innerHTML = Array(num).fill("<span></span>").join("");
+  
+});
+
+textarea.addEventListener("keydown", (event) => {
+  if (event.key === "Tab") {
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    textarea.value =
+      textarea.value.substring(0, start) +
+      "\t" +
+      textarea.value.substring(end);
+    event.preventDefault();
+  }
+});
